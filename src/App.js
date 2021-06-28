cd535ce8e8c26135c01c2a4c80aefa6acbd0a9cf
이종원 <per1215@naver.com>
2021-06-28 23:38:17 +0900

    add Button.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import styled, {css}from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import Button from "./components/Button";
 
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: ${props => props.color};
-  border-radius: 50%;
-  ${props => props.huge && css`
-    width:10rem;
-    height:10rem
-  `}
-`
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Circle color='black' />
-      <Circle color='blue' huge/>
-    </div>
+    <AppBlock>
+      <Button>BUTTON</Button>
+    </AppBlock>
   );
 }
 
